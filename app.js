ec0c4c6179b39945ba21baa34333d783ac6695b4
EbunClef <64669789+EbunClef@users.noreply.github.com>
2023-02-14 13:42:49 +0100

    restart

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
 // Add a class to the header when the user scrolls down
 window.addEventListener("scroll", function () {
-    const header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 0);
+  const header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+// Smooth scroll to the anchor links
+const links = document.querySelectorAll("nav a");
+
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+
+  scroll({
+    top: offsetTop,
+    behavior: "smooth"
   });
-  
-  // Smooth scroll to the anchor links
-  const links = document.querySelectorAll("nav a");
-  
-  for (const link of links) {
-    link.addEventListener("click", clickHandler);
-  }
-  
-  function clickHandler(e) {
-    e.preventDefault();
-    const href = this.getAttribute("href");
-    const offsetTop = document.querySelector(href).offsetTop;
-  
-    scroll({
-      top: offsetTop,
-      behavior: "smooth"
-    });
-  }
-  
+}
